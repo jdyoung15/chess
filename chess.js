832c3fc8b046601c04ce2a5a5b458a9d2bd8320c
Jeremy Young <jedayoung@gmail.com>
2017-10-28 12:20:14 -0500

    Implemented remaining pawn moves

--- chess.js
+++ chess.js
@@ -11,8 +11,18 @@ var Chess = function() {
     WHITE: 1,
     BLACK: 2,
     properties: {
-      1: {name: "white", value: 1},
-      2: {name: "black", value: 2}
+      1: {
+        name: "white", 
+        PAWN_START_ROW: 6, 
+        PAWN_DIRECTION: -1,
+        value: 1
+      },
+      2: {
+        name: "black", 
+        PAWN_START_ROW: 1, 
+        PAWN_DIRECTION: 1,
+        value: 2
+      }
     }
   };
 
@@ -29,34 +39,38 @@ var Chess = function() {
         value: 1,
         findValidMoves: function(position, board) {
           const validMoves = [];
+          const color = board[position].color;
+
           // move up (or down) one row
-          let newPosition = calculateNewPosition(position, -1, 0);
-          if (newPosition > 0 && newPosition < 64 && board[newPosition] === null) {
+          let newPosition = calculateNewPosition(position, 1 * ColorEnum.properties[color].PAWN_DIRECTION, 0);
+          if (newPosition >= 0 && newPosition < 64 && board[newPosition] === null) {
             validMoves.push(newPosition);
           }
 
           // if at start position
-          //if (inStartRow(position)) {
-          //  // move up (or down) two rows
-          //  newPosition = calculateNewPosition(position, -2, 0);
-          //  if (newPosition > 0 && newPosition < 64 && board[newPosition] === null) {
-          //    validMoves.push(newPosition);
-          //  }
-          //}
+          if (this.inStartRow(position, board)) {
+            // move up (or down) two rows
+            newPosition = calculateNewPosition(position, 2 * ColorEnum.properties[color].PAWN_DIRECTION, 0);
+            if (newPosition >= 0 && newPosition < 64 && board[newPosition] === null) {
+              validMoves.push(newPosition);
+            }
+          }
             
           // if can capture opponent
-          //newPosition = calculateNewPosition(position, -1, -1);
-          //if (newPosition > 0 && newPosition < 64/* && board[newPosition] === null)*/) {
-          //  validMoves.push(newPosition);
-          //}
-          //newPosition = calculateNewPosition(position, -1, 1);
-          //if (newPosition > 0 && newPosition < 64/* && board[newPosition] === null)*/) {
-          //  validMoves.push(newPosition);
-          //}
-          //   move up (or down) one row and left/right one column
-          // remove any moves outside the board boundaries 
+          newPosition = calculateNewPosition(position, 1 * ColorEnum.properties[color].PAWN_DIRECTION, -1);
+          if (newPosition >= 0 && newPosition < 64 && containsOpponent(color, newPosition, board)) {
+            validMoves.push(newPosition);
+          }
+          newPosition = calculateNewPosition(position, 1 * ColorEnum.properties[color].PAWN_DIRECTION, 1);
+          if (newPosition > 0 && newPosition < 64 && containsOpponent(color, newPosition, board)) {
+            validMoves.push(newPosition);
+          }
           return validMoves;
         }, 
+        inStartRow: function(position, board) {
+          const color = board[position].color;
+          return ColorEnum.properties[color].PAWN_START_ROW === findRow(position);
+        },
       },
       2: {name: "knight", value: 2},
       3: {name: "bishop", value: 3},
@@ -66,10 +80,18 @@ var Chess = function() {
     }
   };
 
+  function containsOpponent(color, position, board) {
+    return board[position] && board[position].color !== color;
+  }
+
+  function findRow(position) {
+    return Math.floor(position / 8);
+  }
+
   // may return null
   function calculateNewPosition(oldPosition, rows, cols) {
     let newPosition = oldPosition + rows * 8 + cols;
-    if (newPosition <= 0 || newPosition >= 64) {
+    if (newPosition < 0 || newPosition >= 64) {
       newPosition = null;
     }
     return newPosition;
@@ -80,16 +102,14 @@ var Chess = function() {
   }
   
   function isValidMove(startPosition, endPosition, board) {
+    // todo check that piece exists at start position
     return findValidMoves(startPosition, board).includes(endPosition);
   }
 
   // returns a list of positions where the piece at the given position can move
   function findValidMoves(position, board) {
     const piece = board[position];
-    return PieceTypeEnum.properties[1].findValidMoves(position, board);
-    // if no piece at position
-    //   return []\
-    // 
+    return PieceTypeEnum.properties[piece.type].findValidMoves(position, board);
   }
 
   return {
