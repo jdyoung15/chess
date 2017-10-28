1cdc500b55a4c7fae3251d1ac8b2dd699a60c8ef
Jeremy Young <jedayoung@gmail.com>
2017-10-27 20:28:25 -0500

    Found alternate way to implement alternate squares; adjusted square borders

--- chess.js
+++ chess.js
@@ -1,22 +1,10 @@
 var Chess = function() {
 
-  function isEven(num) {
-    return num % 2 === 0;
-  }
-
-  function isAlternateSquare(position) {
-    const row = Math.floor(position / 8);
-    if (isEven(row)) {
-      // if row is even and position is odd: true
-      return !isEven(position);
-    } else {
-      // if row is odd and position is even: true
-      return isEven(position);
-    }
-  }
+  //function isEven(num) {
+  //  return num % 2 === 0;
+  //}
 
   return {
-    isAlternateSquare: isAlternateSquare
   }
 
 }()
