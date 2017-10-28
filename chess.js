4ea5c46ddeafe56826355d7d9232d466da8dc5cc
Jeremy Young <jedayoung@gmail.com>
2017-10-28 10:42:44 -0500

    Rough implementation for moving pieces by clicking

--- chess.js
+++ chess.js
@@ -17,7 +17,40 @@ var Chess = function() {
     QUEEN: 5,
     KING: 6,
     properties: {
-      1: {name: "pawn", value: 1},
+      1: {
+        name: "pawn", 
+        value: 1,
+        findValidMoves: function(position, board) {
+          const validMoves = [];
+          // move up (or down) one row
+          let newPosition = calculateNewPosition(position, -1, 0);
+          if (newPosition > 0 && newPosition < 64 && board[newPosition] === null) {
+            validMoves.push(newPosition);
+          }
+
+          // if at start position
+          //if (inStartRow(position)) {
+          //  // move up (or down) two rows
+          //  newPosition = calculateNewPosition(position, -2, 0);
+          //  if (newPosition > 0 && newPosition < 64 && board[newPosition] === null) {
+          //    validMoves.push(newPosition);
+          //  }
+          //}
+            
+          // if can capture opponent
+          //newPosition = calculateNewPosition(position, -1, -1);
+          //if (newPosition > 0 && newPosition < 64/* && board[newPosition] === null)*/) {
+          //  validMoves.push(newPosition);
+          //}
+          //newPosition = calculateNewPosition(position, -1, 1);
+          //if (newPosition > 0 && newPosition < 64/* && board[newPosition] === null)*/) {
+          //  validMoves.push(newPosition);
+          //}
+          //   move up (or down) one row and left/right one column
+          // remove any moves outside the board boundaries 
+          return validMoves;
+        }, 
+      },
       2: {name: "knight", value: 2},
       3: {name: "bishop", value: 3},
       4: {name: "rook", value: 4},
@@ -26,18 +59,37 @@ var Chess = function() {
     }
   };
 
+  // may return null
+  function calculateNewPosition(oldPosition, rows, cols) {
+    let newPosition = oldPosition + rows * 8 + cols;
+    if (newPosition < 0 || newPosition >= 64) {
+      newPosition = null;
+    }
+    return newPosition;
+  }
+
   function findPieceImgName(type, color) {
     return "images/" + PieceTypeEnum.properties[type].name + "-" + ColorEnum.properties[color].name + ".png";
   }
+  
+  function isValidMove(startPosition, endPosition, board) {
+    return findValidMoves(startPosition, board).includes(endPosition);
+  }
 
-  //function isEven(num) {
-  //  return num % 2 === 0;
-  //}
+  // returns a list of positions where the piece at the given position can move
+  function findValidMoves(position, board) {
+    const piece = board[position];
+    return PieceTypeEnum.properties[1].findValidMoves(position, board);
+    // if no piece at position
+    //   return []\
+    // 
+  }
 
   return {
     ColorEnum: ColorEnum,
     PieceTypeEnum: PieceTypeEnum,
-    findPieceImgName: findPieceImgName
+    findPieceImgName: findPieceImgName,
+    isValidMove: isValidMove,
   }
 
 }()
